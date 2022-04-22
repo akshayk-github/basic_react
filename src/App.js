8ee89579651e5141226884072a05acb421cd9f8b
akshayk-github <akshaykumbhani1919@gmail.com>
2022-04-22 10:53:29 +0530

    22-04

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Akshay
+          Learn React by akshay
         </a>
       </header>
     </div>
