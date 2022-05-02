7c8c6c5535c4f1f0362b6496c09687609dca62ec
akshayk-github <akshaykumbhani1919@gmail.com>
2022-05-02 11:07:27 +0530

    map-filter-reduce

--- src/App.js
+++ src/App.js
@@ -3,105 +3,84 @@ import './App.css';
 
 function App() {
 
-  let initialValue = 0
-  const medicine = [
-    {
-      id: 101,
-      name: 'Abacavir',
-      quantity: 25,
-      price: 150,
-      expiry: 2022,
-      status: true
-    },
-    {
-      id: 102,
-      name: 'Eltrombopag',
-      quantity: 90,
-      price: 550,
-      expiry: 2021,
-      status: true
-    },
-    {
-      id: 103,
-      name: 'Meloxicam',
-      quantity: 85,
-      price: 450,
-      expiry: 2025,
-      status: false
-    },
-    {
-      id: 104,
-      name: 'Allopurinol',
-      quantity: 50,
-      price: 600,
-      expiry: 2023,
-      status: true
-    },
-    {
-      id: 105,
-      name: 'Phenytoin',
-      quantity: 63, 
-      price: 250,
-      expiry: 2021,
-      status: false
-    }
-  ]
-  .reduce(
-    (previousValue, currentValue) => previousValue + currentValue.price
-    , initialValue
-  )
-
-  console.log(medicine) 
-
-  console.log(medicine.map((i) => {return i}));
-
-  console.log(medicine.filter((i) => i.expiry > 2021));
-
-
-  // let initialValue = 0
-  // let sum = [
-  //   {
-  //     price: 1,
-  //     y: 1
-  //   },
-  //   {
-  //     price: 3,
-  //     y: 1
-  //   },
-  //   {
-  //     price: 1,
-  //     y: 1
-  //   }
-  // ]
-  // .reduce(
-  //     (previousValue, currentValue) => previousValue + currentValue.price
-  //     , initialValue
-  // )
-  
-  // console.log(sum) 
+    const medicine = [
+        {
+            id: 101,
+            name: 'Abacavir',
+            quantity: 25,
+            price: 150,
+            expiry: 2022,
+            status: true
+        },
+        {
+            id: 102,
+            name: 'Eltrombopag',
+            quantity: 90,
+            price: 550,
+            expiry: 2021,
+            status: true
+        },
+        {
+            id: 103,
+            name: 'Meloxicam',
+            quantity: 85,
+            price: 450,
+            expiry: 2025,
+            status: false
+        },
+        {
+            id: 104,
+            name: 'Allopurinol',
+            quantity: 50,
+            price: 600,
+            expiry: 2023,
+            status: true
+        },
+        {
+            id: 105,
+            name: 'Phenytoin',
+            quantity: 63,
+            price: 250,
+            expiry: 2021,
+            status: false
+        }
+    ];
 
 
 
+    // medicine.map((value, index) => console.log(value.id, value.name));
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    let filterdata = medicine.filter((d, i) => d.expiry >= 2022);
 
-        </a>
-      </header>
-    </div>
-  );
+    let ans = filterdata.reduce((acc, d, i) => acc + d.price, 0);
+
+    // let ans = medicine.reduce((acc, d, i) => acc + d.price, 0);
+    // console.log(ans);
+
+    return (
+        <>
+            <table>
+                <th>
+                    <td>Id</td>
+                    <td>Name</td>
+                    <td>Price</td>
+                    <td>Expiry</td>
+                </th>
+                {
+                    filterdata.map((value, index) => {
+                        return (
+                            <tr>
+                                <td>{value.id}</td>
+                                <td>{value.name}</td>
+                                <td>{value.price}</td>
+                                <td>{value.expiry}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+        </>
+    );
 }
 
 export default App;
