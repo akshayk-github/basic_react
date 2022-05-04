e0def2f525246b6605b84482c1cc52e9684515d3
akshayk-github <akshaykumbhani1919@gmail.com>
2022-05-04 10:32:04 +0530

    projact 1 circlet

--- src/App.js
+++ src/App.js
@@ -3,115 +3,71 @@ import './App.css';
 
 function App() {
 
-    const medicine = [
+    const Employee = [
         {
-            id: 101,
-            name: 'Abacavir',
-            quantity: 25,
-            price: 150,
-            expiry: 2022,
-            status: true
+          name: "amit",
+          age: 35,
+          salary: 40000,
+          bonus: 1000,
+          status: true
         },
         {
-            id: 102,
-            name: 'Eltrombopag',
-            quantity: 90,
-            price: 550,
-            expiry: 2021,
-            status: true
+          name: "ajay",
+          age: 25,
+          salary: 38000,
+          bonus: 2000,
+          status: false
         },
         {
-            id: 103,
-            name: 'Meloxicam',
-            quantity: 85,
-            price: 450,
-            expiry: 2025,
-            status: false
+          name: "mayur",
+          age: 23,
+          salary: 50000,
+          bonus: 500,
+          status: true
         },
         {
-            id: 104,
-            name: 'Allopurinol',
-            quantity: 50,
-            price: 600,
-            expiry: 2023,
-            status: true
+          name: "jay",
+          age: 29,
+          salary: 35000,
+          bonus: 600,
+          status: true
         },
         {
-            id: 105,
-            name: 'Phenytoin',
-            quantity: 63,
-            price: 250,
-            expiry: 2021,
-            status: false
+          name: "raj",
+          age: 33,
+          salary: 22000,
+          bonus: 2000,
+          status: true
         }
-    ];
-
-    // medicine.map((value, index) => console.log(value.id, value.name));
-
-    let filterdata = medicine.filter((d, i) => d.expiry >= 2022);
-
-    // let ans = filterdata.reduce((acc, d, i) => acc + d.price, 0);
-
-    let ans = medicine.reduce((acc, d, i) => acc + d.price, 0);
-    // console.log(ans);
+      ]
 
 
+     let filterdata = Employee.filter((d) => d.status == true);
 
-
-    let arr = [10,20,30,40,50];
-
-    // copy
-    let arr1 = [...arr];
-    console.log(arr1);
-
-    // merge
-    let arr2 = [99, ...arr, 60];
-    console.log(arr2);
-
-    // destructure
-    let [maths, eng, sci, guj, ss] = arr;
-    console.log(ss);
-
-
-    // copy
-    let obj ={
-        id: 101,
-        name: 'akshay'
-    };
-    let obj1 = {...obj};
-    console.log(obj1);
-
-    // merge
-    let obj2 = {
-        ...obj,
-        place: 'surat'
-    };
-    console.log(obj2);
-
-    // destructure
-    let {id, name} = obj;
-    console.log(id, name);
-
+     let ans = filterdata.reduce((acc, d, i) => acc + d.salary+d.bonus, 0);
+ 
 
     return (
         <>
             <table border="1">
                 <tr>
-                    <th>Id</th>
                     <th>Name</th>
-                    <th>Price</th>
-                    <th>Expiry</th>
+                    <th>age</th>
+                    <th>Salary</th>
+                    <th>Bonus</th>
                     <th>total</th>
+                    <th>final total</th>
                 </tr>
                 {
                     filterdata.map((value, index) => {
                         return (
                             <tr>
-                                <td>{value.id}</td>
                                 <td>{value.name}</td>
-                                <td>{value.price}</td>
-                                <td>{value.expiry}</td>
-                                { index ===  0? <td rowspan = {medicine.length}>{ans}</td> :null }
+                                <td>{value.age}</td>
+                                <td>{value.salary}</td>
+                                <td>{value.bonus}</td>
+                                <td>{value.salary+value.bonus}</td>
+                                { index ===  0? <td rowspan = {Employee.length}>{ans}</td> : null }
                             </tr>
                         )
                     })
