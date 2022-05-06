b8f6521aad663c043402639169c531b57c2523fc
akshayk-github <akshaykumbhani1919@gmail.com>
2022-05-06 10:48:51 +0530

    add

--- src/container/Conuntry_fun.js
+++ src/container/Conuntry_fun.js
@@ -2,20 +2,29 @@ import React, { useState } from 'react';
 
 function Conuntry_fun(props) {
 
-    const [country, setCountry] = useState('India'),
-    [GDP, setgdp] = useState('2')
+    const [country, setCountry] = useState('India')
+    const [king, setKing] = useState('Narendra Modi')
+    const [Dushman, setDushman] = useState('Pakistan')
+    const [city, setCity] = useState('Gujarat')
 
     const changecountry = () => {
         setCountry('Russia')
+        setKing('Vladimir Putin')
+        setDushman('Ukraine')
+        setCity('Moscow')
     }
 
     return (
         <div>
-            <p>Country [function based Component]: {country}</p>
+            <h1>Function based Component</h1>
+            <p>Country : {country}</p>
+            <p>King : {king}</p>
+            <p>Dushman : {Dushman}</p>
+            <p>City : {city}</p>
             <button onClick={() => changecountry()}>change</button>
 
-            <p>GDP : {GDP}</p>
-            <button onClick={() => setgdp(5.5)}>Change</button>
+            {/* <p>GDP : {GDP}</p>
+            <button onClick={() => setgdp(5.5)}>Change</button> */}
 
         </div>
     );
